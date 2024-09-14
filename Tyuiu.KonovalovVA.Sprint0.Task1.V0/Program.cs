bbf64167413e0bdafe626357a5aa69f375e2db59
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-09-14 16:42:43 +0500

    Task1

---
+++ Tyuiu.KonovalovVA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.KonovalovVA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Игорь";
+            int age = 18;
+            Console.WriteLine("Здравстуйте, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
