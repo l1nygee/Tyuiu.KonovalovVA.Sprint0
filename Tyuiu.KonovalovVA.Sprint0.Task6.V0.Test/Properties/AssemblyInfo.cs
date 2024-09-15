b8eb30e75f371d3ed6e3dc8752145cde99bf81a6
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-09-15 23:01:53 +0500

    Task6

---
+++ Tyuiu.KonovalovVA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonovalovVA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonovalovVA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c9b3c05-dc08-421f-9fa9-5b400de9e0ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.KonovalovVA.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.KonovalovVA.Sprint0.Task6.V0/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Tyuiu.KonovalovVA.Sprint0.Task6.V0
+﻿using Tyuiu.KonovalovVA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KonovalovVA.Sprint0.Task6.V0
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numsArray = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionalArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultArray(numsArray));
+            Console.ReadKey();
         }
     }
 }
