d92def45c4ea5614de50de135b242ca42e54166b
Владимир Коновалов <Tyuiu.KonovalovVA@outlook.com>
2024-09-15 13:09:43 +0500

    Task5

--- Tyuiu.KonovalovVA.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.KonovalovVA.Sprint0.Task5.V0/Program.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.KonovalovVA.Sprint0.Task5.V0
             Console.WriteLine("A + B = " + DataService.Additional(1, 5));
             Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
             Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
-            Console.WriteLine("A / B = " + DataService.Division(5, 3));
+            Console.WriteLine("A / B = " + DataService.Division(5, 0));
             Console.ReadKey();
         }
     }
